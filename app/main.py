92378ed68f9b173fe96c3262451b72e3a43e6c80
Abdul Rehman <ar618211@gmail.com>
2026-07-22 20:09:06 +0500

    feat: sqlmodels heroes example with res body

--- app/main.py
+++ app/main.py
@@ -1,13 +1,34 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from pydantic import BaseModel
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-app = FastAPI()
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    secret_name: str
+    age: int | None = Field(default=None, index=True)
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool | None = None
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
@@ -15,11 +36,20 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/heroes", response_model=list[Hero])
+def read_heroes(q: str | None = None):
+    with Session(engine) as session:
+        statement = select(Hero)
+        if q:
+            statement = statement.where(Hero.name == q)
+        heroes = session.exec(statement).all()
+        return {"heroes": heroes}
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.post("/heroes", response_model=Hero)
+def create_hero(hero: Hero):
+    with Session(engine) as session:
+        session.add(hero)
+        session.commit()
+        session.refresh(hero)
+        return hero
